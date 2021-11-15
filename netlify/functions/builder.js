bc531f51f08fb0d93a7e62b9a73972a6f982f1c7
Emily Zhang <emilyzhang@netlify.com>
2021-11-15 14:13:24 -0800

    what

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -3,12 +3,12 @@ const { builder } = require("@netlify/functions");
 function handler(event, context) {
   return {
     statusCode: 200,
-    ttl: 3600,
     headers: {
       "Content-Type": "text/html",
       Etag: "12345",
       "Cache-Control": "max-age=100,no-store",
     },
+    ttl: 3600,
     body: `
     <!DOCTYPE html>
 	    <html>
