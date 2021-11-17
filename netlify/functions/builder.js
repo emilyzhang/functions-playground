e03db44e3de65f4d95a0d4d202ae44f8d1fed2c8
Emily Zhang <emilyzhang@netlify.com>
2021-11-16 17:17:49 -0800

    test ttl functionality

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -5,18 +5,11 @@ async function handler(event, context) {
     statusCode: 200,
     headers: {
       "Content-Type": "text/html",
-      Etag: "12345",
-      "Cache-Control": "max-age=100,no-store",
     },
-    ttl: 3600,
-    body: `
-    <!DOCTYPE html>
-	    <html>
-		    <body>
-		      Hello World 1
-		    </body>
-    </html>
-    `,
+    ttl: 1,
+    body: JSON.stringify({
+      timestamp: Date.now(),
+    }),
   };
 }
 
