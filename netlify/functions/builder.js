2505b1d1c4afc22a630361a41df02ed962a0af10
Emily Zhang <emilyzhang@netlify.com>
2021-11-16 17:47:29 -0800

    something fishy

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -6,7 +6,7 @@ async function handler(event, context) {
     headers: {
       "Content-Type": "text/html",
     },
-    ttl: 60,
+    ttl: 61,
     body: JSON.stringify({
       timestamp: new Date(),
     }),
