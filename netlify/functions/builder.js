2f518bc691c8573d8dd1a03fd6408a249f40ee5d
Emily Zhang <emilyzhang@netlify.com>
2021-11-16 18:43:59 -0800

    what

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -6,7 +6,7 @@ async function handler(event, context) {
     headers: {
       "Content-Type": "text/html",
     },
-    ttl: 61,
+    ttl: 5,
     body: JSON.stringify({
       timestamp: new Date(),
     }),
