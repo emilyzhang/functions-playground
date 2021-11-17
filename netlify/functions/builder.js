625d363971cc8d6f9ced365f2b9d2d1f9dbf8b15
Emily Zhang <emilyzhang@netlify.com>
2021-11-17 01:16:24 -0800

    try headers

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -6,7 +6,7 @@ async function handler(event, context) {
     headers: {
       "Content-Type": "text/html",
     },
-    ttl: event.path,
+    ttl: event.headers["ttl"],
     body: JSON.stringify({
       timestamp: new Date(),
       random_number: Math.random(),
