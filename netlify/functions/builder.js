1debd6861426be1b865a02c9fb0c59d02ff7e3e8
Emily Zhang <emilyzhang@netlify.com>
2021-11-17 16:24:38 -0800

    secrets?

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -2,22 +2,6 @@ const { builder, NetlifySecrets, getSecrets } = require("@netlify/functions");
 
 async function handler(event, context) {
   let secrets = {};
-  secrets = await getSecrets(event);
-  if (secrets.github) {
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html",
-      },
-      ttl: parseInt(event.headers["ttl"], 10),
-      body: JSON.stringify({
-        // timestamp: new Date(),
-        random_number: Math.random(),
-        ttl: parseInt(event.headers["ttl"], 10),
-        secret: "SECRET EXISTS!!!!",
-      }),
-    };
-  }
   return {
     statusCode: 200,
     headers: {
@@ -28,7 +12,7 @@ async function handler(event, context) {
       // timestamp: new Date(),
       random_number: Math.random(),
       ttl: parseInt(event.headers["ttl"], 10),
-      secret: "NO SECRET",
+      secret: secrets,
     }),
   };
 }
