ba65897149e0f3103f05d113e9aefb86e44ea1b9
Emily Zhang <emilyzhang@netlify.com>
2021-11-17 16:20:43 -0800

    check that auth still works

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -1,6 +1,23 @@
-const { builder } = require("@netlify/functions");
+const { builder, NetlifySecrets, getSecrets } = require("@netlify/functions");
 
 async function handler(event, context) {
+  let secrets = {};
+  secrets = await getSecrets(event);
+  if (secrets.github) {
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      ttl: parseInt(event.headers["ttl"], 10),
+      body: JSON.stringify({
+        // timestamp: new Date(),
+        random_number: Math.random(),
+        ttl: parseInt(event.headers["ttl"], 10),
+        secret: "SECRET EXISTS!!!!",
+      }),
+    };
+  }
   return {
     statusCode: 200,
     headers: {
