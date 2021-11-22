ca7fb5c1f795c3f36f9a30d9a1984bb8462b5421
Emily Zhang <emilyzhang@netlify.com>
2021-11-22 12:30:03 -0800

    new deploy

--- netlify/functions/builder.js
+++ netlify/functions/builder.js
@@ -2,6 +2,7 @@ const { builder, NetlifySecrets, getSecrets } = require("@netlify/functions");
 
 async function handler(event, context) {
   let secrets = {};
+
   secrets = await getSecrets(event);
   return {
     statusCode: 200,
@@ -10,8 +11,8 @@ async function handler(event, context) {
     },
     ttl: parseInt(event.headers["ttl"], 10),
     body: JSON.stringify({
-      // timestamp: new Date(),
-      random_number: Math.random(),
+      timestamp: new Date(),
+      random: Math.random(),
       ttl: parseInt(event.headers["ttl"], 10),
     }),
   };
